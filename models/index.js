856af65ffb8e1852d1e0801fcfe7305cae0c08e4
dmmontoto <dmontoto@ucsd.edu>
2023-09-21 19:15:33 -0700

    added Comment.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,25 @@
 const User = require('./User');
 const Plan = require('./Plan');
+const Comment = require('./Comment');
 
-module.exports = { User, Plan };
+module.exports = { User, Plan, Comment };
+
+User.hasMany(Plan, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Plan.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Plan.hasMany(Comment, {
+    foreignKey: 'id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(Plan, {
+    foreignKey: 'id'
+})
+
+module.exports = { User, Plan, Comment };
