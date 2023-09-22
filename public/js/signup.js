8ae8f34a5725884d6774c1bf7010733c550a111a
dmmontoto <dmontoto@ucsd.edu>
2023-09-21 20:43:43 -0700

    Revert "fixed seed error"
    This reverts commit 7c4cf76ccee38e6830c3c2f754a799966777558a.

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,7 +5,7 @@ const signupFormHandler = async (event) => {
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
+    if (email && password) {
       const response = await fetch('/api/users/signup', {
         method: 'POST',
         body: JSON.stringify({ name, email, password }),
